49a83f667811cf6a9aef599f0371e19bc9b95555
Fany Ortiz <fanyortiz1204@gmail.com>
2026-03-04 18:07:33 -0600

    fix: agregar rutas de productos

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import authRoutes from './routes/auth.routes.js';
 import authBasicRoutes from './routes/authBasicRoutes.js';
+import productRoutes from './routes/productRoutes.js';
 import pool from './config/db.js';
 
 const app = express();
@@ -34,6 +35,7 @@ app.use('/api/auth', authBasicRoutes);
 // Autenticación biométrica (WebAuthn)
 app.use('/api/auth', authRoutes);
 
+app.use('/api/products', productRoutes);
 /* ============================
    RUTA BASE
 ============================ */
