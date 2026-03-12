1b64b21fe89781c61dec54491d53f0f9cc6cd712
Fany Ortiz <fanyortiz1204@gmail.com>
2026-03-11 18:19:37 -0600

    add webauthn complete registration

--- backend/src/routes/webauthnRoutes.js
+++ backend/src/routes/webauthnRoutes.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import { 
-  registerOptions,      // 👈 antes era generateRegistration
-  verifyRegistration     // 👈 este sí está bien
+  generateRegistration,
+  verifyRegistration
 } from '../controllers/webauthnController.js';
 
 const router = express.Router();
 
-// Ruta para INICIAR registro
-router.post('/register/begin', registerOptions);  // 👈 actualizado
+// iniciar registro biométrico
+router.post('/register/begin', generateRegistration);
 
-// Ruta para COMPLETAR registro
+// completar registro biométrico
 router.post('/register/complete', verifyRegistration);
 
 export default router;
